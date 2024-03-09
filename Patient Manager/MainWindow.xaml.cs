3cd86760949e4a7fddde10629440c07d5acdae64
CarlMurray <carl.j.murray@gmail.com>
2024-03-09 12:32:51 +0000

    add classes and functionality

--- Patient Manager/MainWindow.xaml.cs
+++ Patient Manager/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace Patient_Manager
 {
@@ -16,11 +22,76 @@ namespace Patient_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Patient> patientList { get; set; }
+        public ObservableCollection<Ward> wardList { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            patientList = new ObservableCollection<Patient>();
+            wardList = new ObservableCollection<Ward>();
+
+            patientListBox.ItemsSource = patientList;
+            wardListBox.ItemsSource = wardList;
+
+            patientList.Add(new Patient("carl murray", new DateTime(1990, 2, 2), Patient.Blood_Type.A));
+            patientList.Add(new Patient("john doe", new DateTime(1993, 2, 2), Patient.Blood_Type.AB));
+
+            wardList.Add(new Ward("Adult's ward", 9, patientList));
+            wardList.Add(new Ward("Children's ward", 6, patientList));
+
+            var jsonfile = JsonSerializer.Serialize(patientList);
+            File.WriteAllText($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/data.json", jsonfile);
+
         }
 
+        // Add Ward on click handler
+        private void AddWardBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            wardList.Add(new Ward(addWardName.Text.Trim(), (int)addWardCapacity.Value));
+        }
 
+        // Enable/disable addWardBtn
+        private void AddWardName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(((TextBox)sender).Text))
+            {
+                addWardBtn.IsEnabled = true;
+            }
+            else addWardBtn.IsEnabled = false;
+        }
+
+        private void AddPatientBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Patient.Blood_Type bloodType;
+            if (Radio_BloodType_A.IsChecked== true)
+            {
+                bloodType = Patient.Blood_Type.A;
+            }
+            else if (Radio_BloodType_B.IsChecked ==true)
+            {
+                bloodType= Patient.Blood_Type.B;
+            }
+            else if (Radio_BloodType_AB.IsChecked==true)
+            {
+                bloodType = Patient.Blood_Type.AB;
+            }
+            else
+            {
+                bloodType = Patient.Blood_Type.O;
+            }
+
+            patientList.Add(new Patient(addPatientName.Text.Trim(), addPatientDOB.DisplayDate, bloodType));
+        }
+
+        private void AddPatientName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(((TextBox)sender).Text))
+            {
+                addPatientBtn.IsEnabled = true;
+            }
+            else addPatientBtn.IsEnabled = false;
+        }
     }
 }
---
+++ Patient Manager/Patient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patient_Manager
+{
+    public class Patient
+    {
+        public string Name { get; set; }
+        private DateTime _dateOfBirth;
+
+
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Date of birth cannot be in the future.");
+                }
+                _dateOfBirth = value;
+                Age = (int)Math.Floor((DateTime.Now - _dateOfBirth).TotalDays / 365);
+            }
+        }
+        public int Age { get; private set; }
+        public Blood_Type BloodType { get; set; }
+        public string printPatient => $"{Name} ({Age}) Type {BloodType}";
+
+
+        public Patient(string name, DateTime dateOfBirth, Blood_Type bloodType)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            BloodType = bloodType;
+
+
+        }
+
+        public enum Blood_Type
+        {
+            A, B, AB, O
+        }
+    }
+}
